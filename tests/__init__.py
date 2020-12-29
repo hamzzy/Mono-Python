55903088785f10f83e9e9f0cf2c50497661fc8ee
ibrahim hamzat <ibrahimhamzat@ibrahims-MacBook-Pro.local>
2020-12-29 20:07:53 +0100

    gitnore update and readme updated

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,4 +4,4 @@ from unittest import TestCase, main
 from pymono import BaseAPI, Mono
 
 test_mono_key = os.getenv('MONO-SEC-KEY')
-os.environ['MONO-SEC-KEY'] = "6te27yg7ast6two96t9s"
+os.environ['MONO-SEC-KEY'] = " "
