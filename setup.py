351b90762fe2a7b166a5ded9e47ac6278c904103
ibrahim hamzat <ibrahimhamzat@ibrahims-MacBook-Pro.local>
2020-12-29 15:03:56 +0100

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
-from setuptools import setup
+from setuptools import setup,find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
 
 setup(
     name='Mono_Python',
-    version='0.1',
-    packages=['src'],
-    url='',
-    license='',
-    author='ibrahimhamzat',
+    version='0.0.1',
+    author='Ibrahim Hamzat',
     author_email='hamat.ibrahim3@gmail.com',
-    description=''
+    description="A mono client package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hamzzy/",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
